7e7a56028ff1402e5413d2535abe711042f920cb
Unword112 <pog123@hotmail.co.uk>
2022-08-20 21:30:58 +0700

    'Day-2'

--- Reverse Number.js
+++ Reverse Number.js
@@ -1,15 +1,25 @@
 const reverse = (number) => {
-    return(
-        parseFloat(
-            number.toString().split('').reverse('').join('')
-        ) * Math.sign(number)
-    )
+    let rev = parseFloat(
+                number
+                    .toString()
+                    .split('')
+                    .reverse('')
+                    .join('')
+                ) * Math.sign(number);
+
+    if(rev < (Math.pow(2,31) * -1) || rev > (Math.pow(2,31) - 1)) {
+        return 0;
+    }
+    
+    return rev;
 }
 
 var x = 123;
 var y = -123;
-var z = 54321;
+var z = -2143847412;
+var w = 5120345689;
 
 console.log(reverse(x));
 console.log(reverse(y));
 console.log(reverse(z));
+console.log(reverse(w));
---
+++ String to Interger.js
@@ -0,0 +1,40 @@
+const StringToInteger = (number) => {
+    let trim = number.replace(/[a-zA-Z]/g, '');
+    let arr = number.split('');
+    let str = parseInt(trim);
+    let letters = /[a-z.+]/i;
+
+    let POSITIVE_INTEGER = Math.pow(2,31);
+    let NEGATIVE_INTEGER = Math.pow(2,31) |0;
+
+    if(arr[0].match(letters)) return 0;
+    else if(arr[0].match(/[-]/i) && arr[1].match(/[+]/i)) return 0;
+
+    if(str > POSITIVE_INTEGER){
+        return POSITIVE_INTEGER;
+    } else if( str < NEGATIVE_INTEGER){
+        return NEGATIVE_INTEGER;
+    }
+
+    return str;
+}
+
+const x = '42';
+const y = '   42';
+const z = '42 with word';
+const w = 'words and 987';
+const a = '   -42'
+const b = '-+12';
+const c = '-11';
+const d = '-91283472332';
+const e = '91283472332'
+
+console.log(StringToInteger(x));
+console.log(StringToInteger(y));
+console.log(StringToInteger(z));
+console.log(StringToInteger(w));
+console.log(StringToInteger(a));
+console.log(StringToInteger(b));
+console.log(StringToInteger(c));
+console.log(StringToInteger(d));
+console.log(StringToInteger(e));
