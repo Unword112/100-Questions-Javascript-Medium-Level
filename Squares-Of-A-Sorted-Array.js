1bb16a0767e0d141a9d335b101781d70e8a8b713
Unword112 <pog123@hotmail.co.uk>
2022-08-29 12:34:45 +0700

    'Day-8'

---
+++ Squares-Of-A-Sorted-Array.js
@@ -0,0 +1,15 @@
+var sortedSquares = function(nums) {
+    if(nums === null || nums.length === 0) return false;
+    
+    var arr = [];
+    for(let i = 0; i < nums.length; i++ ){
+        nums[i] = Math.pow(nums[i], 2);
+        arr.push(nums[i]);
+    }
+    return arr.sort((a, b) => a - b);
+};
+
+const test1 = sortedSquares([-7,-3,2,3,11]);
+const test2 = sortedSquares([-4,-1,0,3,10]);
+
+console.log(test1, test2);
