51fd89950d315672c562ec97fb9e0c2460bece96
unword112 <pog123@hotmail.co.uk>
2022-09-01 19:54:51 +0700

    'Day-10'

--- Middle-of-the-Linked-List.js
+++ Middle-of-the-Linked-List.js
@@ -1,10 +1,16 @@
 var middleNode = (head) => {
-    console.log(head.splice(head.length / 2));
+    var left = head;
+    var right = head;
 
+    while(right != null && right.next != null){
+        left = left.next;
+        right = right.next.next;
+    }
+    return left;
 }
 
-var test1 = [1, 2 ,3, 4, 5];
-var test2 = [1, 2, 3, 4, 5, 6];
+var test1 = middleNode([1, 2 ,3, 4, 5]);
+var test2 = middleNode([1, 2, 3, 4, 5, 6]);
 
-console.log(middleNode(test1));
-console.log(middleNode(test2));
+console.log(test1);
+console.log(test2);
