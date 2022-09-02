2821484f9567296420d59133da05e6e88b1a5b11
unword112 <pog123@hotmail.co.uk>
2022-09-02 11:03:30 +0700

    'Day-11'

--- Longest-Substring-Without-Repeating-Characters.js
+++ Longest-Substring-Without-Repeating-Characters.js
@@ -1,6 +1,16 @@
 var longthOfLongestSubstring = (s) => {
-    var arr = s.split("");
-    console.log(arr)
+    var maxLength = 0;
+    var a = 0;
+    for(let i = 0; i < s.length; i++){
+        for(let j = a; j <i; j++){
+            if(s[i] === s[j]) {
+                a = j + 1;
+                break;
+            }
+        }
+    if(i - a + 1 > maxLength) maxLength = i - a + 1;
+    }
+    return maxLength;
 }
 
 var test1 = longthOfLongestSubstring("abcabcbb");
