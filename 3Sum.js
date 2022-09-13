167cf4abf01a89053abe10f19f441cba76ad0913
Unword112 <pog123@hotmail.co.uk>
2022-09-13 18:30:09 +0700

    'Day-19'

---
+++ 3Sum.js
@@ -0,0 +1,34 @@
+var threeSum = (nums) => {
+    nums.sort((a,b) => a-b)
+	const result=[]
+
+	if(nums.length < 3) return result
+
+	for(let i=0; i< nums.length; i++){
+		let left = i+1
+		let right = nums.length-1
+		if(i > 0 && nums[i] === nums[i-1]) continue
+		while(left < right){
+			let sum = nums[i] + nums[left] + nums[right]
+
+			if(sum===0){
+				let arr=[nums[i], nums[left], nums[right]]
+				result.push(arr)
+				left++
+				while(nums[left] === nums[left-1] && left < right) left++
+			}else if(sum > 0){
+				right--
+			}else{
+				left++
+			}
+		}
+	}
+
+	return result
+};
+
+const test1 = threeSum([-1,0,1,2,-1,-4]);
+const test2 = threeSum([0,1,1]);
+const test3 = threeSum([0,0,0]);
+
+console.log(test1, test2, test3);
